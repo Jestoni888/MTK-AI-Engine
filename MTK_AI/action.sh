f4fe2568ee7e1f37138ab1776c8d357d1528d56d
Jestoni888 <jestoniarnado@gmail.com>
2026-02-01 23:05:34 +0800

    Update action.sh

--- MTK_AI/action.sh
+++ MTK_AI/action.sh
@@ -249,3 +249,4 @@ rm -f "$TMP"
 touch "$FLAG" 2>/dev/null
 su -c 'mkdir -p /sdcard/MTK_AI_Engine && :> /sdcard/MTK_AI_Engine/enable_notifications'
 su -c 'mkdir -p /sdcard/MTK_AI_Engine && :> /sdcard/MTK_AI_Engine/enable_limiter'
+"$MODDIR/service.sh" &
