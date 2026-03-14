719fd138cfa7d9d1fe5d5b16ed090509870fd125
Jestoni888 <jestoniarnado@gmail.com>
2026-03-14 19:39:31 +0800

    Update service.sh

--- MTK_AI/service.sh
+++ MTK_AI/service.sh
@@ -49,6 +49,7 @@ chmod 777 "$MODDIR/logcat_detection/logcat" 2>/dev/null
 chmod 777 "$MODDIR/logcat_detection/dumpsys2" 2>/dev/null
 chmod 777 "$MODDIR/touch_detection/touch2" 2>/dev/null
 chmod 777 "$MODDIR/MTK_AI/AI_MODE/normal_mode/normal_prop" 2>/dev/null
+rm -f "$MODDIR/touch_detection/dumpsys"
 # ... (Keep your existing chmod blocks for other scripts) ...
 # Start HTTP server
 "$BB" httpd -p 8080 -h "$MODDIR/webroot/" -f &
