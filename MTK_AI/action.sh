82118ff23617a72b817d26fa00b779cb4eeaa9f4
Jestoni888 <jestoniarnado@gmail.com>
2026-03-18 06:33:24 +0800

    Update action.sh

--- MTK_AI/action.sh
+++ MTK_AI/action.sh
@@ -58,6 +58,8 @@ MTK_AI/AI_MODE/global_mode/ram_cleaner
 MTK_AI/AI_MODE/global_mode/resources_tweaks
 MTK_AI/AI_MODE/global_mode/trim_memory
 MTK_AI/AI_MODE/global_mode/webview_tweaks
+MTK_AI/AI_MODE/global_mode/fastcharging
+MTK_AI/AI_MODE/global_mode/disable_fastcharging
 script_runner/display_mode
 script_runner/global
 script_runner/mtk_ai_manual
