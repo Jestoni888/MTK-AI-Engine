6cf1fb3f003f19339f759c485bef401f71af7ef5
Jestoni888 <jestoniarnado@gmail.com>
2026-03-23 01:16:13 +0800

    Add files via upload

---
+++ MTK_AI/touch_detection/touch2.sh
@@ -0,0 +1,4 @@
+# /data/adb/modules/MTK_AI/touch_detection/touch2.sh
+#!/system/bin/sh
+export LD_LIBRARY_PATH=/data/adb/modules/MTK_AI/lib64:$LD_LIBRARY_PATH
+exec /data/adb/modules/MTK_AI/touch_detection/touch2
