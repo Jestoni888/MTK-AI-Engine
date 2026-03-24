9b32fa0bd0714170e1e0e8ca217503f8e40d163f
Jestoni888 <jestoniarnado@gmail.com>
2026-03-24 14:17:18 +0800

    Delete MTK_AI/logcat_detection directory

--- MTK_AI/logcat_detection/logcat.sh
+++
@@ -1,4 +0,0 @@
-# /data/adb/modules/MTK_AI/logcat_detection/logcat.sh
-#!/system/bin/sh
-export LD_LIBRARY_PATH=/data/adb/modules/MTK_AI/lib64:$LD_LIBRARY_PATH
-exec /data/adb/modules/MTK_AI/logcat_detection/logcat
