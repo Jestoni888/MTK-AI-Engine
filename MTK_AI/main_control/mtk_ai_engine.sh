c41bd334fdb669f71a22c0a0e071203d28c39dfd
Jestoni888 <jestoniarnado@gmail.com>
2026-05-24 05:51:23 +0800

    Add files via upload

--- MTK_AI/main_control/mtk_ai_engine.sh
+++ MTK_AI/main_control/mtk_ai_engine.sh
@@ -1,4 +1,8 @@
 # /data/adb/modules/MTK_AI/main_control/mtk_ai_engine
 #!/system/bin/sh
+
+STATE="/sdcard/MTK_AI_Engine/automode"
+ENGINE="/data/adb/modules/MTK_AI/main_control/mtk_ai_engine"
 export LD_LIBRARY_PATH=/data/adb/modules/MTK_AI/lib64:$LD_LIBRARY_PATH
-exec /data/adb/modules/MTK_AI/main_control/mtk_ai_engine
+mkdir -p /sdcard/MTK_AI_Engine
+[ "$(cat "$STATE" 2>/dev/null | tr -d '[:space:]')" = "1" ] && exec "$ENGINE"
