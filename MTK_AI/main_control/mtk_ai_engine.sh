2cfbccf11ba8cfb2d22ae0a10860aa903f14942f
Jestoni888 <jestoniarnado@gmail.com>
2026-06-11 01:08:09 +0800

    Update mtk_ai_engine.sh

--- MTK_AI/main_control/mtk_ai_engine.sh
+++ MTK_AI/main_control/mtk_ai_engine.sh
@@ -8,4 +8,4 @@ DAEMON="mtk_ai_engine"
 ps 2>/dev/null | grep -v grep | grep -qw "$DAEMON" && exit 0
 
 export LD_LIBRARY_PATH=/data/adb/modules/MTK_AI/lib64:$LD_LIBRARY_PATH
-exec "/data/adb/modules/MTK_AI/main_control/$DAEMON"
+nohup "/data/adb/modules/MTK_AI/main_control/$DAEMON"
