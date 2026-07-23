bd8e178e98233574e1db85cb4e0740592a51ec01
Jestoni888 <jestoniarnado@gmail.com>
2026-07-23 10:48:50 +0800

    Add files via upload

--- MTK_AI/webroot2/mtk_ai_engine.js
+++ MTK_AI/webroot2/mtk_ai_engine.js
@@ -25,6 +25,7 @@ const scriptPaths = {
     enable_module: { on: null, off: null, isDaemon: false, critical: false },
     fast_mode_switch: { on: null, off: null, isDaemon: false, critical: false },
     use_tweaks: { on: null, off: null, isDaemon: false, critical: false },
+    random_device: { on: null, off: null, isDaemon: false, critical: false },
     enable_cpu: { on: null, off: null, isDaemon: false, critical: false },
     enable_lite_gaming: { on: null, off: null, isDaemon: false, critical: false },
     disable_tap_to_rotate: { on: null, off: null, isDaemon: false, critical: false }
@@ -60,6 +61,7 @@ const toggleConfig = {
     enable_module: { title:  "Module automation ", desc:  "No need to reboot when new modules installed except system level modules ", group:  "System " },
     fast_mode_switch: { title:  "Fast Mode Switch ", desc:  "Quickly switch between normal, gaming  & screen state profiles ", group:  "System " },
     use_tweaks: { title:  "Use tweaks ", desc:  "Using tweaks between normal mode & gaming mode (⚠️lite mode) ", group:  "System " },
+    random_device: { title:  "Random device ", desc:  "Randomize device name & Mac address in wireless network to bypass from being blocked 🚫 ", group:  "System " },
     enable_cpu: { title:  "Auto shutdown CPU 6  & 7 ", desc:  "Auto shutdown cpu 6  & 7 when temp reaches 42°C  & auto revert in 39°C ", group:  "Battery " },
     enable_lite_gaming: { title:  "Lite Gaming Mode ", desc:  "Lightweight tweaks for low-end devices ", group:  "Gaming " },
     disable_tap_to_rotate: { title:  "Disable Tap to Rotate ", desc:  "Hide the manual rotation button on the navigation bar ", group:  "Display " }
