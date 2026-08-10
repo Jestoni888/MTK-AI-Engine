17a2100a65e4786361e75f432d6372f93cd34f1a
Jestoni888 <jestoniarnado@gmail.com>
2026-08-10 15:57:45 +0800

    Update mtk_ai_engine.js

--- MTK_AI/webroot2/mtk_ai_engine.js
+++ MTK_AI/webroot2/mtk_ai_engine.js
@@ -65,7 +65,7 @@ const toggleConfig = {
     use_tweaks: { title:  "Use tweaks ", desc:  "Using tweaks between normal mode & gaming mode (⚠️lite mode) ", group:  "System " },
     random_device: { title:  "Random device ", desc:  "Randomize device name & Mac address in wireless network to bypass from being blocked 🚫 (⚠️turn off wifi first & wait 5-10 seconds) ", group:  "System " },
     limit_surfaceflinger: { title:  "Limit Surfaceflinger", desc:  "Limit Surfaceflinger in normal use to reduce overheating 🔥", group:  "System " },
-    enable_cpu: { title:  "Auto shutdown CPU 6  & 7 ", desc:  "Auto shutdown cpu 6  & 7 when temp reaches 42°C  & auto revert in 39°C ", group:  "Battery " },
+    enable_cpu: { title:  "Hexacore mode ", desc:  "Auto shutdown cpu 6  & 7 when temp reaches 42°C  & auto revert in 39°C ", group:  "Battery " },
     enable_lite_gaming: { title:  "Lite Gaming Mode ", desc:  "Lightweight tweaks for low-end devices ", group:  "Gaming " },
     suspend_bg: { title:  "Suspend background apps", desc:  "Suspend background apps to smoother gaming experience ‼️ Enable USE TWEAKS first ‼️", group:  "Gaming " },
     disable_tap_to_rotate: { title:  "Disable Tap to Rotate ", desc:  "Hide the manual rotation button on the navigation bar ", group:  "Display " }
