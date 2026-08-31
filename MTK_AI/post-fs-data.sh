c9b15f48ef622c51510fbd489ab307abc2d33089
Jestoni888 <jestoniarnado@gmail.com>
2026-08-31 14:33:18 +0800

    Update post-fs-data.sh

--- MTK_AI/post-fs-data.sh
+++ MTK_AI/post-fs-data.sh
@@ -85,6 +85,20 @@ backup_part() {
     fi
 }
 
+default_vm() {
+OUT="/sdcard/MTK_AI_Engine/restore_vm.sh"
+
+echo '#!/bin/bash' > "$OUT"
+sysctl -a 2>/dev/null | grep '^vm\.' | while read -r line; do
+    key=$(echo "$line" | cut -d= -f1 | xargs)
+    val=$(echo "$line" | cut -d= -f2- | xargs)
+    echo "sysctl -w ${key}=\"${val}\"" >> "$OUT"
+done
+
+chmod +x "$OUT"
+echo "Created $OUT"
+}
+
 # 5. Execute backups
 if [ -f "/sdcard/MTK_AI_Engine/backup_rom" ]; then
             backup_part "boot" "boot*"
