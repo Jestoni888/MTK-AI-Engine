8dc908048ee4f4e78c8b5bd42bc77dc881b75451
Jestoni888 <jestoniarnado@gmail.com>
2026-09-13 09:26:53 +0800

    Update post-fs-data.sh

--- MTK_AI/post-fs-data.sh
+++ MTK_AI/post-fs-data.sh
@@ -2,8 +2,12 @@
 
 OUT="/data/adb/modules/MTK_AI/MTK_AI/AI_MODE/normal_mode/stock_tweaks.sh"
 
-echo '#!/bin/bash' > "$OUT"
-sysctl -a 2>/dev/null | grep '^vm\.' | while read -r line; do
+# Note: Changed to #!/system/bin/sh for better Android compatibility, 
+# as some devices don't have /bin/bash symlinked by default.
+echo '#!/system/bin/sh' > "$OUT"
+
+# Search for BOTH vm. and kernel.sched_ parameters
+sysctl -a 2>/dev/null | grep -E '^(vm\.|kernel\.sched_)' | while read -r line; do
     key=$(echo "$line" | cut -d= -f1 | xargs)
     val=$(echo "$line" | cut -d= -f2- | xargs)
     echo "sysctl -w ${key}=\"${val}\"" >> "$OUT"
