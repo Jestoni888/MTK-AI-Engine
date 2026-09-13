8325265d57c3c58e57a1c2d1f34aaf132487f90d
Jestoni888 <jestoniarnado@gmail.com>
2026-09-13 10:54:36 +0800

    Update post-fs-data.sh

--- MTK_AI/post-fs-data.sh
+++ MTK_AI/post-fs-data.sh
@@ -6,11 +6,23 @@ OUT="/data/adb/modules/MTK_AI/MTK_AI/AI_MODE/normal_mode/stock_tweaks.sh"
 # as some devices don't have /bin/bash symlinked by default.
 echo '#!/system/bin/sh' > "$OUT"
 
-# Search for BOTH vm. and kernel.sched_ parameters
+# 1. Search for BOTH vm. and kernel.sched_ parameters
 sysctl -a 2>/dev/null | grep -E '^(vm\.|kernel\.sched_)' | while read -r line; do
     key=$(echo "$line" | cut -d= -f1 | xargs)
     val=$(echo "$line" | cut -d= -f2- | xargs)
     echo "sysctl -w ${key}=\"${val}\"" >> "$OUT"
+done
+
+# 2. Backup currently active thermal services
+echo "" >> "$OUT"
+echo "# --- Active Thermal Services Backup ---" >> "$OUT"
+
+# Dynamically find all init services containing 'thermal' that are currently 'running'
+# awk extracts the property name, sed removes the 'init.svc.' prefix to get the pure service name
+getprop | grep 'init.svc.' | grep 'thermal' | grep '\[running\]' | awk -F'[][]' '{print $2}' | sed 's/^init\.svc\.//' | while read -r svc; do
+    if [ -n "$svc" ]; then
+        echo "start $svc" >> "$OUT"
+    fi
 done
 
 chmod +x "$OUT"
