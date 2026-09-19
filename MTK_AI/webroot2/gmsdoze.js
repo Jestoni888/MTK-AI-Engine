fe09d17ee76cc014b02296767d998083722c72ef
Jestoni888 <jestoniarnado@gmail.com>
2026-09-19 20:12:04 +0800

    Update gmsdoze.js

--- MTK_AI/webroot2/gmsdoze.js
+++ MTK_AI/webroot2/gmsdoze.js
@@ -9,6 +9,7 @@
         'com.google.android.gsf',
         'com.google.android.gsf.login',
         'com.google.android.gms.persistent',
+        'com.google.android.gms.unstable',
         'com.google.android.backuptransport',
         'com.google.android.partnersetup',
         'com.google.android.setupwizard'
