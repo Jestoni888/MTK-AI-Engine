c4521e7cd18e6a2a5c8287006f1e7bb0f05634e8
Jestoni888 <jestoniarnado@gmail.com>
2026-09-23 17:51:52 +0800

    Add files via upload

--- MTK_AI/action.sh
+++ MTK_AI/action.sh
@@ -123,11 +123,6 @@ thermal
 trimmer
 hibernation
 
-main
-thermal
-trimmer
-hibernation
-
 # === 1. Detect module dir ===
 detect_moddir() {
 [ -d "/data/adb/modules/MTK_AI" ] && { echo "/data/adb/modules/MTK_AI"; return; }
