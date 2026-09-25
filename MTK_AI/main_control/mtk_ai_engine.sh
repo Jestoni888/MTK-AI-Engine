f3f4e520f2d0cb1c4eeea4ba05c8399d62747521
Jestoni888 <jestoniarnado@gmail.com>
2026-09-25 18:56:57 +0800

    Update mtk_ai_engine.sh

--- MTK_AI/main_control/mtk_ai_engine.sh
+++ MTK_AI/main_control/mtk_ai_engine.sh
@@ -5,3 +5,5 @@ ps|grep -v grep|grep -qw $D&&exit 0
 export LD_LIBRARY_PATH=/data/adb/modules/MTK_AI/lib64:$LD_LIBRARY_PATH
 setsid /data/adb/modules/MTK_AI/main_control/$D >/dev/null 2>&1&
 setsid /data/adb/modules/MTK_AI/script_runner/mtk_ai_manual >/dev/null 2>&1&
+setsid /data/adb/modules/MTK_AI/script_runner/boost_color_apply.sh >/dev/null 2>&1&
+setsid /data/adb/modules/MTK_AI/MTK_AI/AI_MODE/normal_mode/powersavex >/dev/null 2>&1&
