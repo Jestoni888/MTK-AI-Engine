b7efd305c086d1909d60d238eb5538e58d6eccf0
Jestoni888 <jestoniarnado@gmail.com>
2026-09-26 07:49:15 +0800

    Update mtk_ai_engine.sh

--- MTK_AI/main_control/mtk_ai_engine.sh
+++ MTK_AI/main_control/mtk_ai_engine.sh
@@ -8,4 +8,5 @@ setsid /data/adb/modules/MTK_AI/main_control/$D >/dev/null 2>&1&
 setsid /data/adb/modules/MTK_AI/script_runner/mtk_ai_manual >/dev/null 2>&1&
 setsid /data/adb/modules/MTK_AI/script_runner/boost_color_apply.sh >/dev/null 2>&1&
 setsid /data/adb/modules/MTK_AI/MTK_AI/AI_MODE/normal_mode/powersavex >/dev/null 2>&1&
+setsid /data/adb/modules/MTK_AI/MTK_AI/AI_MODE/gaming_mode/thermalx >/dev/null 2>&1&
 resolution
